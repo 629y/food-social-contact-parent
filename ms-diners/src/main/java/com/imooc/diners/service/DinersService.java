1d73442308964e00de0a88e04b0c2ccdbbf5afa1
zxy <zxy06291@gmail.com>
2024-03-06 02:26:58 +0800

    1.食客服务业务逻辑层   登录

---
+++ ms-diners/src/main/java/com/imooc/diners/service/DinersService.java
@@ -0,0 +1,86 @@
+package com.imooc.diners.service;
+
+import cn.hutool.core.bean.BeanUtil;
+import com.imooc.commons.constant.ApiConstant;
+import com.imooc.commons.model.domain.ResultInfo;
+import com.imooc.commons.utils.AssertUtil;
+import com.imooc.diners.config.OAuth2ClientConfiguration;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.*;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+/**
+ * 食客服务业务逻辑层
+ */
+@Service
+public class DinersService {
+    @Resource
+    private RestTemplate restTemplate;
+
+    @Value("${service.name.ms-oauth-server}")
+    private String oauthServiceName;
+
+    @Resource
+    private OAuth2ClientConfiguration oAuth2ClientConfiguration;
+
+    /**
+     * 登录
+     * @param account  帐号：用户名或手机或邮箱
+     * @param password 密码
+     * @param path     请求路径
+     * @return
+     */
+    public ResultInfo signIn(String account,String password,String path){
+        // 1.参数校验
+        AssertUtil.isNotEmpty(account,"请输入登录账号");
+        AssertUtil.isNotEmpty(password,"请输入登录密码");
+        // 2.构建请求头
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        // 3.构建请求体（请求参数）
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("username",account);
+        body.add("password",password);
+        body.setAll(BeanUtil.beanToMap(oAuth2ClientConfiguration));
+        HttpEntity<MultiValueMap<String, Object>> entity = new HttpEntity<>(body, headers);
+        // 4.设置 Authorization
+        restTemplate.getInterceptors()
+                .add(new BasicAuthenticationInterceptor(oAuth2ClientConfiguration.getClientId(),
+                        oAuth2ClientConfiguration.getSecret()));
+        // 5.发送请求
+        ResponseEntity<ResultInfo> result = restTemplate
+                .postForEntity(oauthServiceName + "oauth/token", entity, ResultInfo.class);
+        // 6.处理返回结果
+        AssertUtil.isTrue(result.getStatusCode() != HttpStatus.OK,"登录失败");
+        ResultInfo resultInfo = result.getBody();
+        if (resultInfo.getCode() != ApiConstant.SUCCESS_CODE){
+            // 登录失败
+            resultInfo.setData(resultInfo.getMessage());
+            return resultInfo;
+        }
+        //
+        resultInfo.getData();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
