5cd838b5b33d4add63525a55004065cf2f449a07
zxy <zxy06291@gmail.com>
2024-03-05 23:38:18 +0800

    1.Rest 配置类

---
+++ ms-diners/src/main/java/com/imooc/diners/config/RestTemplateConfiguration.java
@@ -0,0 +1,19 @@
+package com.imooc.diners.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * Rest 配置类
+ */
+@Configuration
+public class RestTemplateConfiguration {
+
+    @LoadBalanced
+    @Bean
+    public RedisTemplate redisTemplate(){
+        return new RedisTemplate();
+    }
+}
