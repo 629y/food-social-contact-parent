ad21c8333b6a996b8967094a5d91e9ef2500613c
zxy <zxy06291@gmail.com>
2024-03-12 01:45:01 +0800

    1.用户中心

---
+++ ms-diners/src/main/java/com/imooc/diners/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.imooc.diners.controller;
+
+import cn.hutool.core.bean.BeanUtil;
+import com.imooc.commons.model.domain.ResultInfo;
+import com.imooc.commons.model.domain.SignInIdentity;
+import com.imooc.commons.model.vo.SignInDinerInfo;
+import com.imooc.commons.utils.ResultInfoUtil;
+import org.springframework.beans.BeanUtils;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 用户中心
+ */
+@RestController
+public class UserController {
+
+    @Resource
+    private HttpServletRequest request;
+
+    @GetMapping("user/me")
+    public ResultInfo getCurrentUser(Authentication authentication){
+        // 获取登录用户的信息，然后设置
+        SignInIdentity signInIdentity = (SignInIdentity) authentication.getPrincipal();
+        // 转为前端可用的视图对象
+        SignInDinerInfo dinerInfo = new SignInDinerInfo();
+        BeanUtils.copyProperties(signInIdentity,dinerInfo);
+        return ResultInfoUtil.buildSuccess(request.getServletPath(),dinerInfo);
+    }
+
+
+
+
+
+
+
+
+
+
+}
