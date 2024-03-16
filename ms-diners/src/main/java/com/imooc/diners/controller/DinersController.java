8c8ca90e743600c9f6af5ab020ff6fa7714e583c
zxy <zxy06291@gmail.com>
2024-03-17 05:43:20 +0800

    1.完成注册   Controller     注册

--- ms-diners/src/main/java/com/imooc/diners/controller/DinersController.java
+++ ms-diners/src/main/java/com/imooc/diners/controller/DinersController.java
@@ -1,10 +1,13 @@
 package com.imooc.diners.controller;
 
 import com.imooc.commons.model.domain.ResultInfo;
+import com.imooc.commons.model.dto.DinersDTO;
 import com.imooc.commons.utils.ResultInfoUtil;
 import com.imooc.diners.service.DinersService;
 import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -22,6 +25,16 @@ public class DinersController {
 
     @Resource
     private HttpServletRequest request;
+
+    /**
+     * 注册
+     * @param dinersDTO
+     * @return
+     */
+    @PostMapping("register")
+    public ResultInfo register(@RequestBody DinersDTO dinersDTO){
+        return dinersService.register(dinersDTO, request.getServletPath());
+    }
 
     /**
      * 校验手机号是否已注册
